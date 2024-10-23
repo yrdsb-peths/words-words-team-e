06fc9dceb561fbb35f7419ad8bbc84a5dab16467
TheLostone145 <martinthegamer145@gmail.com>
2024-10-23 12:56:59 -0400

    Gave button picture

--- Button.java
+++ Button.java
@@ -7,7 +7,7 @@ public class Button extends Actor
     public Button(Runnable action) 
     {
         this.action = action;
-        GreenfootImage image = new GreenfootImage("images/buttonLong_beige.png");
+        GreenfootImage image = new GreenfootImage("images/pngtree-blue-game-button-png-image_5402465.jpg");
         setImage(image);
     }
 
