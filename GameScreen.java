bae6577afb2b0bcf06971becf39ec78ae32c0c02
TheLostone145 <martinthegamer145@gmail.com>
2024-10-25 12:40:24 -0400

    Testing Scanner

--- GameScreen.java
+++ GameScreen.java
@@ -1,8 +1,16 @@
 import greenfoot.*;
-
+import java.util.Scanner;
 public class GameScreen extends World {
     public GameScreen() {
         super(600, 400, 1);
+        try (Scanner myObj = new Scanner(System.in)) {
+            String userName;
+   
+            System.out.println("Enter username"); 
+            userName = myObj.nextLine();   
+      
+            System.out.println("Username is: " + userName);
+        }  
     }
     
 }
