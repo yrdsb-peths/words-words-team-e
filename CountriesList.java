ac92ee4ae1380bd080d60b7dba6bdd8c0aa83bb6
350444535 <150150007+350444535@users.noreply.github.com>
2024-10-25 13:15:27 -0400

    added lists for the three themes

---
+++ CountriesList.java
@@ -0,0 +1,103 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CountriesList here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CountriesList extends World
+{
+    public String[] countries = 
+    {
+        "algeria",
+        "america",
+        "argentina",
+        "australia",
+        "austria",
+        "bahamas",
+        "bangladesh",
+        "barbados",
+        "belarus",
+        "belgium",
+        "belize",
+        "brazil",
+        "canada",
+        "chad",
+        "chile",
+        "china",
+        "colombia",
+        "comoros",
+        "croatia",
+        "cuba",
+        "denmark",
+        "ecuador",
+        "egypt",
+        "ethiopia",
+        "fiji",
+        "finland",
+        "france",
+        "germany",
+        "ghana",
+        "greece",
+        "guyana",
+        "haiti",
+        "hungary",
+        "iceland",
+        "india",
+        "indonesia",
+        "iraq",
+        "iran",
+        "ireland",
+        "israel",
+        "italy",
+        "jamaica",
+        "japan",
+        "kenya",
+        "latvia",
+        "libya",
+        "lithuania",
+        "malaysia",
+        "mexico",
+        "monaco",
+        "mongolia",
+        "morocco",
+        "myanmar",
+        "nepal",
+        "nigeria",
+        "norway",
+        "pakistan",
+        "panama",
+        "peru",
+        "poland",
+        "portugal",
+        "qatar",
+        "romania",
+        "russia",
+        "serbia",
+        "slovakia",
+        "slovenia",
+        "somalia",
+        "spain",
+        "sudan",
+        "sweden",
+        "syria",
+        "taiwan",
+        "tanzania",
+        "thailand",
+        "tunisia",
+        "turkey",
+        "uganda",
+        "ukraine",
+        "uzbekistan",
+        "venezuela",
+        "vietnam",
+        "yemen",
+        "zambia",
+    };
+    public CountriesList()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
---
+++ FoodList.java
@@ -0,0 +1,93 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FoodList here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FoodList extends World
+{
+    public String[] food = 
+    {
+        "apple",
+        "asparagus",
+        "bagel",
+        "banana",
+        "bean",
+        "beef",
+        "beer",
+        "beet",
+        "brandy",
+        "bread",
+        "broccoli",
+        "bun",
+        "butter",
+        "cabbage",
+        "cake",
+        "carrot",
+        "celery",
+        "cherry",
+        "chocolate",
+        "coffee",
+        "corn",
+        "croissant",
+        "cucumber",
+        "cupcake",
+        "egg",
+        "eggplant",
+        "garlic",
+        "gin",
+        "grape",
+        "hamburger",
+        "honey",
+        "jelly",
+        "juice",
+        "ketchup",
+        "lasagna",
+        "lemonade",
+        "lettuce",
+        "macaroni",
+        "milk",
+        "mimosa",
+        "oatmeal",
+        "olive",
+        "onion",
+        "orange",
+        "pancake",
+        "pasta",
+        "peanut",
+        "pear",
+        "pepper",
+        "pickle",
+        "pie",
+        "pizza",
+        "popcorn",
+        "potato",
+        "pumpkin",
+        "radish",
+        "ravioli",
+        "rice",
+        "rum",
+        "sandwich",
+        "soup",
+        "spaghetti",
+        "spinach",
+        "sugar",
+        "sushi",
+        "tea",
+        "tequila",
+        "toast",
+        "tomato",
+        "turnip",
+        "vodka",
+        "whiskey",
+        "wine",
+        "yogurt",
+    };
+    public FoodList()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
