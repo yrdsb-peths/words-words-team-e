3d9a331fc28a052717d6f85af7779e0b87c39d33
jamie wang <bombyjamie@gmail.com>
2024-10-25 13:15:04 -0400

    Added word spliting code

--- GameScreen.java
+++ GameScreen.java
@@ -1,8 +1,18 @@
 import greenfoot.*;
 
 public class GameScreen extends World {
+    private Label test;
+    //private Label hanger = new(GreenfootImage);
     public GameScreen() {
         super(600, 400, 1);
+
+        String myStr = "cheese";
+        char[] charArray = myStr.toCharArray();
+        for (char s : charArray) {
+            System.out.println(s);
+        
+        }
+
     }
     
 }
