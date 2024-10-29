3e24d9b42e873f132ed929ce4f228d716924bd84
TheLostone145 <martinthegamer145@gmail.com>
2024-10-29 12:44:52 -0400

    testing setting up hangman code

--- GameScreen.java
+++ GameScreen.java
@@ -1,12 +1,23 @@
 import greenfoot.*;
 public class GameScreen extends World {
     String myStr;
+    char firstLetter;
+    char secondLetter;
+    char thirdLetter;
+    char fourthLetter;
+    char fifthLetter;
+    char sixthLetter;
+    char seventhLetter;
+    char eighthLetter;
+    char ninthLetter;
+    int letter=0;
     public int index = Greenfoot.getRandomNumber(74);
     public GameScreen() {
         super(600, 400, 1);
+    
 
         if (MenuScreen.themeType==0){
-            myStr=AnimalList.animals[1];
+            myStr=AnimalList.animals[index];
             char[] charArray = myStr.toCharArray();
             for (char s : charArray) {
                 System.out.println(s);
@@ -15,7 +26,7 @@ public class GameScreen extends World {
         }
 
         else if(MenuScreen.themeType==1) {
-            myStr=FoodList.food[1];
+            myStr=FoodList.food[index];
             char[] charArray = myStr.toCharArray();
             for (char s : charArray) {
                 System.out.println(s);
@@ -24,13 +35,42 @@ public class GameScreen extends World {
         }
 
         else {
-            myStr=CountriesList.countries[1];
+            myStr=CountriesList.countries[index];
             char[] charArray = myStr.toCharArray();
             for (char s : charArray) {
-                System.out.println(s);
+                if(letter==0){
+                    firstLetter = s;
+                }else if(letter==1){
+                    secondLetter = s;
+                }else if(letter==2){
+                    thirdLetter = s;
+                }
+                else if(letter==3){
+                    fourthLetter = s;
+                }
+                else if(letter==4){
+                    fifthLetter = s;
+                }
+                else if(letter==5){
+                    sixthLetter = s;
+                }
+                else if(letter==6){
+                    seventhLetter = s;
+                }
+                else if(letter==7){
+                    eighthLetter = s;
+                }
+                else if(letter==8){
+                    ninthLetter = s;
+                }
+                letter++;
             
             }
+            System.out.println(firstLetter);
+        
         }
+        
     }
+
     
 }
