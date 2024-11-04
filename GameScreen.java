79d280260dc1375b4db22fdee3232f3b3c57d409
TheLostone145 <martinthegamer145@gmail.com>
2024-11-04 12:22:06 -0500

    Added code for player hangman input

--- GameScreen.java
+++ GameScreen.java
@@ -12,6 +12,17 @@ public class GameScreen extends World {
     char ninthLetter;
     char tenthLetter;
     char eleventhLetter;
+    Label letterOne = null;
+    Label letterTwo = null;
+    Label letterThree = null;
+    Label letterFour = null;
+    Label letterFive = null;
+    Label letterSix = null;
+    Label letterSeven = null;
+    Label letterEight = null;
+    Label letterNine = null;
+    Label letterTen = null;
+    Label letterEleven = null;
     int letter=0;
     public int index = Greenfoot.getRandomNumber(74);
     public GameScreen() {
@@ -139,54 +150,56 @@ public class GameScreen extends World {
             }
         
         }
-        Label letterOne = null;
         addObject(letterOne, 150, 200);
-        Label letterTwo = null;
         addObject(letterTwo, 200, 200);
-        Label letterThree = null;
         addObject(letterThree, 250, 200);
-        Label letterFour = null;
         addObject(letterFour, 300, 200);
-        Label letterFive = null;
         addObject(letterFive, 350, 200);
-        Label letterSix = null;
         addObject(letterSix, 400, 200);
-        Label letterSeven = null;
         addObject(letterSeven, 450, 200);
-        Label letterEight = null;
         addObject(letterEight, 500, 200);
-        Label letterNine = null;
         addObject(letterNine, 550, 200);
-        Label letterTen = null;
         addObject(letterTen, 600, 200);
-        Label letterEleven = null;
         addObject(letterEleven, 650, 200);
         addObject(new Button(this::inputMethod, "StartButtonIdle.png",114, 56), 300, 350);
 
     }
     public void inputMethod()
     {
-        char[] charArray = new char[11];
-        charArray[0] = firstLetter;
-        charArray[1] = secondLetter;
-        charArray[2] = thirdLetter;
-        charArray[3] = fourthLetter;
-        charArray[4] = fifthLetter;
-        charArray[5] = sixthLetter;
-        charArray[6] = seventhLetter;
-        charArray[7] = eighthLetter;
-        charArray[8] = ninthLetter;
-        charArray[9] = tenthLetter;
-        charArray[10] = eleventhLetter;
-        String input= Greenfoot.ask("Input");
-        char[] answer=input.toCharArray();
-        for(int i=0; i<charArray.length; i++)
-        {
-            if(answer[0]==charArray[i]){
-                System.out.println("Nice");
+        char[] charArray = { firstLetter, secondLetter, thirdLetter, fourthLetter, fifthLetter, 
+                            sixthLetter, seventhLetter, eighthLetter, ninthLetter, tenthLetter, 
+                            eleventhLetter };
+
+        String input = Greenfoot.ask("Input");
+        if (input.length() == 1) {
+            char answer = input.charAt(0);
+
+            for (int i = 0; i < charArray.length; i++)
+            {
+                if (answer == charArray[i]) {
+
+                    revealLetter(i, answer);
+                    break; 
+                }
             }
         }
-
     }
-    
+
+    public void revealLetter(int index, char letter)
+    {
+        switch (index) {
+            case 0: letterOne.setValue(letter); break;
+            case 1: letterTwo.setValue(letter); break;
+            case 2: letterThree.setValue(letter); break;
+            case 3: letterFour.setValue(letter); break;
+            case 4: letterFive.setValue(letter); break;
+            case 5: letterSix.setValue(letter); break;
+            case 6: letterSeven.setValue(letter); break;
+            case 7: letterEight.setValue(letter); break;
+            case 8: letterNine.setValue(letter); break;
+            case 9: letterTen.setValue(letter); break;
+            case 10: letterEleven.setValue(letter); break;
+            
+        }
+    }
 }
